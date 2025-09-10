c7c51f21dabda765d1faf44ca7e42be4fa76d9dc
ililihayy <lilipushkar15@gmail.com>
2025-09-10 11:39:07 +0300

    fix: add init to schemas

---
+++ app/schemas/__init__.py
@@ -0,0 +1,3 @@
+from .cat import Cat, CatBase, CatCreate, CatInDB, CatUpdate
+from .mission import Mission, MissionBase, MissionCreate, MissionInDB, MissionUpdate
+from .target import Target, TargetBase, TargetCreate, TargetInDB, TargetUpdate
