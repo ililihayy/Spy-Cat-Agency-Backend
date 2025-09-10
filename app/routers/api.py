78f1f0b77caa1145fad506a5685376738c5c3d26
ililihayy <lilipushkar15@gmail.com>
2025-09-10 11:38:36 +0300

    feat: add general router

---
+++ app/routers/api.py
@@ -0,0 +1,9 @@
+# app/routers/api.py
+from fastapi import APIRouter
+from app.routers import cat, mission, target
+
+api_router = APIRouter()
+
+api_router.include_router(cat.router, prefix="/cats", tags=["Cats"])
+api_router.include_router(mission.router, prefix="/missions", tags=["Missions"])
+api_router.include_router(target.router, prefix="/targets", tags=["Targets"])
