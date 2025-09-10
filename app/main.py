727f045cd0a5572485812a23538d05670ed630aa
ililihayy <lilipushkar15@gmail.com>
2025-09-10 11:53:47 +0300

    fix: create database

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from app.routers.api import api_router
+from app.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Spy Cat Agency API")
 
